bebc32a4ccb8487493f1716e7edd4b261990faea
aRonnieAlsop <alsop.arron@gmail.com>
2024-12-30 22:10:35 -0800

    'wip'

--- src/components/Home_Page/HomePage.jsx
+++ src/components/Home_Page/HomePage.jsx
@@ -2,21 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './HomePage.css';
 
-const projects = [
-    { id: 1, name: "Project One", route: "/project/1" },
-    { id: 2, name: "Project Two", route: "/project/2"},
-];
-
 const Home = () => {
     return (
         <div className="content-wrapper">
         <div className="homepage">
             <div className="info-bar">
-                <span className="volume-info"><div className="volume-info">Vol. 12, issue 4</div></span> 
+                <span className="volume-info"><div className="volume-info">Project Portfolio</div></span> 
             </div>
             <header className="header">
                 <div className="title-box">
-                    <h1 className="magazine-title">ARRON ALSOP</h1>
+                    <h1 className="magazine-title">CAT WOMAN</h1>
                     <div className="stacked-boxes">
                     <div className="box">Full-Stack</div>
                     <div className="box">Software</div>
