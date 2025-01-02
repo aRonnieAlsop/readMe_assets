e994f0657ad3c4510ac1c51d0ef37bc30c1b2aec
aRonnieAlsop <alsop.arron@gmail.com>
2025-01-01 19:58:19 -0800

    'wip'

--- src/components/Home_Page/HomePage.jsx
+++ src/components/Home_Page/HomePage.jsx
@@ -5,36 +5,39 @@ import './HomePage.css';
 const Home = () => {
     return (
         <div className="content-wrapper">
-        <div className="homepage">
-            <div className="info-bar">
-                <span className="volume-info">
-                    Project Portfolio
-                </span> 
+            <div className="homepage">
+                <div className="info-bar">
+                    <span className="volume-info">
+                        Project Portfolio
+                    </span>
+                </div>
+                <header className="header">
+                    <div className="title-box">
+                        <h1 className="magazine-title">CAT WOMAN</h1>
+                        <div className="stacked-boxes">
+                            <div className="box">Full-Stack</div>
+                            <div className="box">Software</div>
+                            <div className="box">Engineer</div>
+                        </div>
+                    </div>
+                    <div className="media-container">
+                        <div className="slatted-image">
+                            <img
+                                src="/assets/cat_woman.png"
+                                alt="An image of person of interest."
+                                className="image-strips"
+                            />
+                        </div>
+                        <div className="nav-links">
+                            <Link to="/">Project 1</Link>
+                            <Link to="/projects">Project Deux</Link>
+                            <Link to="/about">Most Amazing Project Ever</Link>
+                            <Link to="/contact">Blog</Link>
+                        </div>
+                    </div>
+                </header>
+
             </div>
-            <header className="header">
-                <div className="title-box">
-                    <h1 className="magazine-title">CAT WOMAN</h1>
-                    <div className="stacked-boxes">
-                    <div className="box">Full-Stack</div>
-                    <div className="box">Software</div>
-                    <div className="box">Engineer</div>
-                </div>
-                </div>
-                <div className="slatted-image">
-                <img
-                    src="/assets/cat_woman.png"
-                    alt="An image of person of interest."
-                    className="image-strips"
-                />
-                </div>
-                <div className="nav-links">
-                        <Link to="/">Project 1</Link>
-                        <Link to="/projects">Project Deux</Link>
-                        <Link to="/about">Most Amazing Project Ever</Link>
-                        <Link to="/contact">Blog</Link>
-                </div>
-            </header>
-        </div>
         </div>
     );
 };
