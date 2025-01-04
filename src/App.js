8e67b3a7e8dcba14a619508e0dcfbc0ed09641b9
aRonnieAlsop <alsop.arron@gmail.com>
2025-01-03 20:43:56 -0800

    'wip'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home_Page/HomePage';
-import ProjectDetail from './components/Project_Split-Screens/ProjectSplitScreens';
+import Projects from './components/Projects/Projects';
+import Websites from './components/Websites/Websites';
+import Blog from './components/Blog/Blog';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/project/:id" component={ProjectDetail} />
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/websites" element={<Websites />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Blog.css';
+
+const Blog = () => {
+    return (
+        <div class-Name="blog-container">
+            <h1>Testing, testing</h1>
+        </div>
+    );
+}
+
+
+export default Blog;
+
+
+
